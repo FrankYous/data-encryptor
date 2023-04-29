f77fe603c59fe752e6650b391c5a21fc26a11d64
FrankYous <f_s_yousefi@yahoo.com>
2023-04-29 22:23:11 +0330

    Add some option to the menu

--- encdec.py
+++ encdec.py
@@ -3,15 +3,30 @@
 
 
 def main():
-    print("Welcome to Enc/Dec software tool! Please choose your command.")
-    print("Commands:")
-    print("--exit")
+    print("Welcome to Enc/Dec software tool!")
 
     while True:
-        command = input()
-
-        if command == "--exit":
-            return
+        print("=================================")
+        print("Please choose your command.")
+        print("Commands:")
+        print("--encrypt")
+        print("--exit")
+        main_command = input().lower()
+        if main_command == "--encrypt":
+            while True:
+                print("=================================")
+                print("Commands:")
+                print("--input [string]")
+                print("--back")
+                encrypt_command = input().lower()
+                if encrypt_command[0:7] == "--input":
+                    break
+                if encrypt_command == "--back":
+                    break
+                else:
+                    print("Error: Command not found.")
+        elif main_command == "--exit":
+            break
         else:
             print("Error: Command not found.")
 
