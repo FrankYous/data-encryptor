bb186a279a0f8769c06160f3c2c07016c938b1ca
FrankYous <f_s_yousefi@yahoo.com>
2023-05-19 13:13:39 +0330

    Add option to encrypt a given string

--- encdec.py
+++ encdec.py
@@ -1,5 +1,8 @@
-#import os
-#from cryptography.fernet import fernet
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+import base64
+from base64 import b64encode
 
 
 def main():
@@ -19,9 +22,32 @@ def main():
                 print("--input [string]")
                 print("--back")
                 encrypt_command = input().lower()
-                if encrypt_command[0:7] == "--input":
-                    break
-                if encrypt_command == "--back":
+                if encrypt_command[0:8] == "--input ":
+                    input_string = encrypt_command[8:]
+                    print (f"Entered string: {input_string}")
+                    print ("Please enter a password:")
+                    password = input()
+                    print (f"Entered password: {password}")
+                    password_bytes = bytes(password, "utf-8")
+                    salt = os.urandom(16)
+                    f = open("salt.txt","w+")
+                    f.write(b64encode(salt).decode("utf-8"))
+                    kdf = Scrypt(
+                        salt=salt,
+                        length=32,
+                        n=2**14,
+                        r=8,
+                        p=1,
+                    )
+                    key = base64.urlsafe_b64encode(kdf.derive(password_bytes))
+                    print (f"Password converted to bytes: {password_bytes}")
+                    print (f"Key: {key}")
+                    f = Fernet(key)
+                    input_string_bytes = bytes(input_string, "utf-8")
+                    print (f"String converted to bytes: {input_string_bytes}")
+                    token = f.encrypt (input_string_bytes)
+                    print (f"Encrypted string: {token}")
+                elif encrypt_command == "--back":
                     break
                 else:
                     print("Error: Command not found.")
@@ -31,4 +57,5 @@ def main():
             print("Error: Command not found.")
 
 
-main()
+if __name__ == "__main__":
+    main()
